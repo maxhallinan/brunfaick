ea7becc029f11a64c851a9bc08916cae850b8dc2
Maximum Hallinan <maxhallinan@gmail.com>
2017-03-27 20:44:47 -0400

    Move test/parser to Mocha/Chai

--- test/unit/parser/parse.js
+++ test/unit/parser/parse.js
@@ -1,86 +1,88 @@
-const test = require('ava');
+const assert = require('chai').assert;
 const parse = require('../../../parser/parse');
 
-test('Converts a list of tokens to an abstract syntax tree', t => {
-  // '+++[>+++[>++[>++++<-]<-]<-]>>>.'
-  const tokens = [
-    { type: 'INCREMENT', },
-    { type: 'INCREMENT', },
-    { type: 'INCREMENT', },
-    { type: 'LOOP_START', },
-    { type: 'MOVE_RIGHT', },
-    { type: 'INCREMENT', },
-    { type: 'INCREMENT', },
-    { type: 'INCREMENT', },
-    { type: 'LOOP_START', },
-    { type: 'MOVE_RIGHT', },
-    { type: 'INCREMENT', },
-    { type: 'INCREMENT', },
-    { type: 'LOOP_START', },
-    { type: 'MOVE_RIGHT', },
-    { type: 'INCREMENT', },
-    { type: 'INCREMENT', },
-    { type: 'INCREMENT', },
-    { type: 'INCREMENT', },
-    { type: 'MOVE_LEFT', },
-    { type: 'DECREMENT', },
-    { type: 'LOOP_END', },
-    { type: 'MOVE_LEFT', },
-    { type: 'DECREMENT', },
-    { type: 'LOOP_END', },
-    { type: 'MOVE_LEFT', },
-    { type: 'DECREMENT', },
-    { type: 'LOOP_END', },
-    { type: 'MOVE_RIGHT', },
-    { type: 'MOVE_RIGHT', },
-    { type: 'MOVE_RIGHT', },
-    { type: 'OUTPUT', },
-  ];
+describe('unit > parser > parse', () => {
+  it('Should generate an abstract syntax tree from a list of tokens.', () => {
+    // '+++[>+++[>++[>++++<-]<-]<-]>>>.'
+    const tokens = [
+      { type: 'INCREMENT', },
+      { type: 'INCREMENT', },
+      { type: 'INCREMENT', },
+      { type: 'LOOP_START', },
+      { type: 'MOVE_RIGHT', },
+      { type: 'INCREMENT', },
+      { type: 'INCREMENT', },
+      { type: 'INCREMENT', },
+      { type: 'LOOP_START', },
+      { type: 'MOVE_RIGHT', },
+      { type: 'INCREMENT', },
+      { type: 'INCREMENT', },
+      { type: 'LOOP_START', },
+      { type: 'MOVE_RIGHT', },
+      { type: 'INCREMENT', },
+      { type: 'INCREMENT', },
+      { type: 'INCREMENT', },
+      { type: 'INCREMENT', },
+      { type: 'MOVE_LEFT', },
+      { type: 'DECREMENT', },
+      { type: 'LOOP_END', },
+      { type: 'MOVE_LEFT', },
+      { type: 'DECREMENT', },
+      { type: 'LOOP_END', },
+      { type: 'MOVE_LEFT', },
+      { type: 'DECREMENT', },
+      { type: 'LOOP_END', },
+      { type: 'MOVE_RIGHT', },
+      { type: 'MOVE_RIGHT', },
+      { type: 'MOVE_RIGHT', },
+      { type: 'OUTPUT', },
+    ];
 
-  const expected = [
-    { type: 'INCREMENT', },
-    { type: 'INCREMENT', },
-    { type: 'INCREMENT', },
-    {
-      type: 'LOOP',
-      body: [
-        { type: 'MOVE_RIGHT', },
-        { type: 'INCREMENT', },
-        { type: 'INCREMENT', },
-        { type: 'INCREMENT', },
-        {
-          type: 'LOOP',
-          body: [
-            { type: 'MOVE_RIGHT', },
-            { type: 'INCREMENT', },
-            { type: 'INCREMENT', },
-            {
-              type: 'LOOP',
-              body: [
-                { type: 'MOVE_RIGHT', },
-                { type: 'INCREMENT', },
-                { type: 'INCREMENT', },
-                { type: 'INCREMENT', },
-                { type: 'INCREMENT', },
-                { type: 'MOVE_LEFT', },
-                { type: 'DECREMENT', },
-              ],
-            },
-            { type: 'MOVE_LEFT', },
-            { type: 'DECREMENT', },
-          ],
-        },
-        { type: 'MOVE_LEFT', },
-        { type: 'DECREMENT', },
-      ],
-    },
-    { type: 'MOVE_RIGHT', },
-    { type: 'MOVE_RIGHT', },
-    { type: 'MOVE_RIGHT', },
-    { type: 'OUTPUT', },
-  ];
+    const expected = [
+      { type: 'INCREMENT', },
+      { type: 'INCREMENT', },
+      { type: 'INCREMENT', },
+      {
+        type: 'LOOP',
+        body: [
+          { type: 'MOVE_RIGHT', },
+          { type: 'INCREMENT', },
+          { type: 'INCREMENT', },
+          { type: 'INCREMENT', },
+          {
+            type: 'LOOP',
+            body: [
+              { type: 'MOVE_RIGHT', },
+              { type: 'INCREMENT', },
+              { type: 'INCREMENT', },
+              {
+                type: 'LOOP',
+                body: [
+                  { type: 'MOVE_RIGHT', },
+                  { type: 'INCREMENT', },
+                  { type: 'INCREMENT', },
+                  { type: 'INCREMENT', },
+                  { type: 'INCREMENT', },
+                  { type: 'MOVE_LEFT', },
+                  { type: 'DECREMENT', },
+                ],
+              },
+              { type: 'MOVE_LEFT', },
+              { type: 'DECREMENT', },
+            ],
+          },
+          { type: 'MOVE_LEFT', },
+          { type: 'DECREMENT', },
+        ],
+      },
+      { type: 'MOVE_RIGHT', },
+      { type: 'MOVE_RIGHT', },
+      { type: 'MOVE_RIGHT', },
+      { type: 'OUTPUT', },
+    ];
 
-  const result = parse(tokens);
+    const result = parse(tokens);
 
-  t.deepEqual(result, expected);
+    assert.deepEqual(result, expected);
+  });
 });
