dc77fd9427ef6631b46abd24a36db6299b49da33
Maximum Hallinan <maxhallinan@gmail.com>
2017-03-30 08:04:29 -0400

    Implement read from file

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 
+const fs = require('fs');
 const meow = require('meow');
 const brunfaick = require('.');
 
@@ -12,6 +13,9 @@ const help = `
     -i, --input
       Input provided to [program] during execution.
 
+    -f, --file
+      Path to a Brainfuck file.
+
   Examples
     $ brunfaick '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.'
       Hello World!
@@ -20,6 +24,7 @@ const help = `
 const opts = {
   alias: {
     i: 'input',
+    f: 'file',
   },
 };
 
@@ -27,5 +32,19 @@ const cli = meow(help, opts);
 
 const program = cli.input[0];
 const input = cli.flags.input;
+const file = cli.flags.file;
 
-console.log(brunfaick(program, input));
+if (file) {
+  fs.readFile(file, 'utf8', (err, program) => {
+    if (err) {
+      throw new Error(err.message);
+    }
+
+    console.log(brunfaick(program, input));
+  });
+}
+
+if (!file) {
+  console.log(brunfaick(program, input));
+}
+
